ec1b9c6c3908f5c954524f78c59c69d93a9fba28
pinchuck <gabrielpinchuck@gmail.com>
2024-09-02 12:14:27 +0100

    feat(theme setup): remove header import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import Header from "./components/core/Header";
-
 export default function Home() {
 
   return (
-
-      
-
       <main className="flex p-4 container min-h-screen mx-auto flex-col items-center justify-between p-24">
       
       </main>
-
   );
 }
