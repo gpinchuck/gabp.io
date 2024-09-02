5c556902dcd3246b17852d46dbe3f4c44d4e05e8
pinchuck <gabrielpinchuck@gmail.com>
2024-09-02 14:42:42 +0100

    feat(holding page): set body height to full

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/core/Header";
+import Header from "./components/core/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`min-h-screen ${inter.className}`}>
         <Header />
         {children}
         </body>
