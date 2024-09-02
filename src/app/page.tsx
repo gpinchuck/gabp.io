6d5a52ba9b1c50c837b5974f35deb6602fba9d97
pinchuck <gabrielpinchuck@gmail.com>
2024-09-02 12:07:56 +0100

    feat(theme setup): remove header from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import Header from "./components/core/Header";
 export default function Home() {
 
   return (
-    <>
-      <Header />
 
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        
+      
+
+      <main className="flex p-4 container min-h-screen mx-auto flex-col items-center justify-between p-24">
+      
       </main>
-    </>
+
   );
 }
