671f3e294ade8dc106e4162385ec9369d22bbd13
pinchuck <gabrielpinchuck@gmail.com>
2024-10-04 12:13:30 +0100

    lint

--- src/components/core/Header/Header.tsx
+++ src/components/core/Header/Header.tsx
@@ -1,6 +1,6 @@
-'use client';
-import { useState } from 'react';
-import headerStyles from './Header.module.css';
+"use client";
+import { useState } from "react";
+import headerStyles from "./Header.module.css";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -13,9 +13,9 @@ const Header = () => {
     <header className={`${headerStyles.header} w-full sticky top-0`}>
       <div className="container mx-auto flex justify-between items-center p-4">
         <div className="container content-centre w-auto">
-          <a href='/'>
-              <h1 className="text-white text-xl font-bold">Gabriel Pinchuck</h1>
-              <h2 className="text-white">Front-End Developer</h2>
+          <a href="/">
+            <h1 className="text-white text-xl font-bold">Gabriel Pinchuck</h1>
+            <h2 className="text-white">Front-End Developer</h2>
           </a>
         </div>
       </div>
